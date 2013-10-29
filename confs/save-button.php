3f8adde48b7e2884cc9445251ecacc88aa65ca2c
joelgarciajr84 <joelgarciajr84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-29 17:05:44 +0000

    VersaoNova
    git-svn-id: https://plugins.svn.wordpress.org/wp-enviroment-beacon/trunk@795335 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- confs/save-button.php
+++ confs/save-button.php
@@ -1,13 +1,13 @@
 <?php 
 
-	// Change save button label on whoami custom post type
+	// Change save button labell on whoami custom post type
 
 	add_filter( 'gettext', 'change_publish_button', 10, 2 );
 
 		function change_publish_button( $translation, $text ) {
 
 		if ( $text == 'Publish' & 'whoami' == get_post_type() ) 
-    	return 'Save This Configuration';
+    	$translation = __('Save This Configuration','wpeb');
 
 		return $translation;
 	}
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://joelgarciajr.com.br
 Tags: servers, host, informations, php, phpinfo, settings
 Requires at least: 3.0
 Tested up to: 3.6
-Stable tag: 1.0
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
